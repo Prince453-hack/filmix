3d942bd4e9e7014ecec07d8b970fcee085ab2159
randomgod <tabishnizami456@gmail.com>
2023-10-11 00:56:38 +0530

    dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Imdb",
-  description: "Movie Seatch App",
+  title: "IMDb",
+  description: "Movie Search App",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
