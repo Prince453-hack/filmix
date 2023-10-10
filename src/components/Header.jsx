aeadc45623481259da3a08a61defb0daf066393f
randomgod <tabishnizami456@gmail.com>
2023-10-10 15:14:56 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,19 @@ import { BsFillInfoCircleFill } from "react-icons/bs";
 
 export default function header() {
   return (
-    <div>
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="">
         <Link href="/">
-          <span>ImdB</span>
-          <span>Clone</span>
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              ImDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
         </Link>
       </div>
     </div>
