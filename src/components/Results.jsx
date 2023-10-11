d151f69364d32d8c83de77b6738e24f8197de26a
randomgod <tabishnizami456@gmail.com>
2023-10-12 00:18:47 +0530

    error

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,9 @@ export default function Results({ results }) {
   return (
     <div>
       {results.map((result) => (
-        <div key={result.id}>{result.original_title}</div>
+        <>
+          <div key={result.id}>{result.original_title}</div>
+        </>
       ))}
     </div>
   );
