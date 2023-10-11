638002a57568b916610d39d06985c388aee099fb
randomgod <tabishnizami456@gmail.com>
2023-10-11 13:33:19 +0530

    genre

---
+++ src/app/about/page.jsx
@@ -0,0 +1,32 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-5 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p className="">
+        Welcome to IMDb Clone – where the magic of movies and TV shows comes to
+        life! We're more than just a website; we're a community of dedicated
+        film enthusiasts and entertainment aficionados. At IMDb Clone, we're on
+        a mission to create the ultimate platform for cinephiles like you.
+      </p>
+      <h1 className="text-2xl font-medium text-amber-600">
+        Our Passion for Entertainment
+      </h1>
+      <p>
+        Our love for movies and television knows no bounds. We believe in the
+        power of storytelling and its ability to inspire, entertain, and connect
+        people from all walks of life. With a shared passion for the silver
+        screen, our team has curated an exceptional collection of cinematic gems
+        for your viewing pleasure.
+      </p>
+      <h1 className="text-2xl font-medium text-amber-600">What Drives Us At</h1>
+      <p>
+        IMDb Clone, we're not just a database of movies and TV shows; we're a
+        hub of information, creativity, and fandom. We're driven by the desire
+        to provide you with an immersive experience, allowing you to explore the
+        vast world of entertainment effortlessly. Whether you're a fan of
+        classic films, the latest blockbusters, or binge-worthy TV series, we've
+        got something for everyone.
+      </p>
+    </div>
+  );
+}
