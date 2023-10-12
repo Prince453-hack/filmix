fc3b1f1c02510cc8c13fa0dcf70d9bc48a774a77
randomgod <tabishnizami456@gmail.com>
2023-10-12 14:22:48 +0530

    updated

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -10,7 +10,7 @@ export default async function page({ params }) {
   console.log(movie);
 
   return (
-    <div className="w-full">
+    <div className="w-full mt-10">
       <div className="p-4 md:pt-4 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -31,8 +31,17 @@ export default async function page({ params }) {
           <h2 className="text-lg mb-3 font-bold">
             {movie.title || movie.name}
           </h2>
-          <p>
-            <span>OverView: </span> {movie.overview}
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">OverView: </span>
+            {movie.overview}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Date Released: </span>
+            {movie.release_date || movie.first_aired_date}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating: </span>
+            {movie.vote_count}
           </p>
         </div>
       </div>
