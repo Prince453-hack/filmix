f7792f4445b69b663c03f1b5ac9a221a9b968f62
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2023-10-13 10:55:42 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,41 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BiLike } from "react-icons/bi";
+import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
-  console.log(
-    `https://image.tmdb.org/t/p/original/${
-      result.poster_path || result.backdrop_path
-    }`
-  );
   return (
-    <div className="cursor-pointer sm:p-3 hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
-          } `}
+          }`}
           width={500}
           height={300}
-          className="sm:rounded-lg group-hover:opacity-80 transition-opacity duration-200"
-          placeholder="blur"
-          blurDataURL="/loading.svg"
-          alt="Image Not Available"
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
           style={{
             maxWidth: "100%",
-            maxHeight: "auto",
+            height: "auto",
           }}
-        />
-
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="image is not available"
+        ></Image>
         <div className="p-2">
-          <p className="line-clamp-2 text-sm">{result.overview}</p>
-          <h2 className="truncate text-lg font-bold py-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold">
             {result.title || result.name}
           </h2>
-          <p className="flex items-center text-sm">
+          <p className="flex items-center">
             {result.release_date || result.first_air_date}
-            <BiLike className="h-5 mr-1 ml-3" />
-            {result.vote_count}
+            <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
           </p>
         </div>
       </Link>
